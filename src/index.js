626889761ad5df7f1ef58faafe59cec2547c5249
wmoon22 <wmoon43@gmail.com>
2025-01-29 13:50:49 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Calculator from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Calculator />
+  </React.StrictMode>
+);
